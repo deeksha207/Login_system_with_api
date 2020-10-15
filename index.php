6269d5a873e401b4b00abc580027cafde542c5df
Deeksha Gupta <54803390+deeksha207@users.noreply.github.com>
2020-10-15 14:34:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,6 @@
 <?php
-session_start();
 include('config.php');
-
-$login='';
-if(isset($_GET["code"])){
-    $token=$google_Client->fetchAccessTokenWithAuthCode($_GET["code"]);
-    if(!isset($token['error'])){
-        $google_Client->setAccessToken($token['access_token']);
-        $_SESSION['access_token']=$token['access_token'];
-        $google_service=new Google_Service_Oauth2($google_Client);
-        $data=$google_service->userinfo->get();
-        if(!empty($data['given_name'])){
-            $_SESSION['user_first_name']=$data['given_name'];
-        }
-        if(!empty($data['family_name'])){
-            $_SESSION['user_last_name']=$data['family_name'];
-        }
-        if(!empty($data['email'])){
-            $_SESSION['user_email']=$data['email'];
-        }
-        if(!empty($data['gender'])){
-            $_SESSION['user_gender']=$data['gender'];
-        }
-        if(!empty($data['picture'])){
-            $_SESSION['user_image']=$data['picture'];
-        }
-    }
-}
-if(!isset($_SESSION['access_token'])){
-    $login='<a href="'.$google_Client->createAuthUrl().'" style:"padding:10px; color:white">Login with Google</a>';
-}
+//include('fbconfig.php');
 ?>
 
 <!DOCTYPE html>
@@ -42,9 +13,12 @@ if(!isset($_SESSION['access_token'])){
     <body>
         <div class="back">
                    <?php
-                   if(!isset($login_f)){
+                   if($login==''){
                        header("location:home.html");
                    }
+                 //if(!isset($login_f)){
+                   //    header('location:home.html');
+                   //}
                    else{
                        ?>
                         <h1>Login Here</h1>
